a78ca98d725d351457c5ff65153dd69fbd2c271f
Leonardo C. Santos <leonardocsantoss@gmail.com>
2016-05-04 11:52:49 -0300

    configs

--- django_rows/settings/settings.py
+++ django_rows/settings/settings.py
@@ -140,11 +140,11 @@ DATETIME_INPUT_FORMATS = ('%d/%m/%Y %H:%M:%S', )
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 # Media
-MEDIA_ROOT = os.path.join(BASE_DIR, 'public', 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Static
-STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Admin Static
